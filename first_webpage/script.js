0c5eabe775bb96adf15d0edea70a17029df58c6b
Paul Tobiloba <48536008+Toby-Codes@users.noreply.github.com>
2020-04-04 17:10:20 +0100

    Revert "Revert "Paul tobiloba 1""

---
+++ first_webpage/script.js
@@ -0,0 +1,13 @@
+var name = "Paul Tobiloba";
+let courses = ["HTML", "CSS", "JAVASCRIPT", "nodeJs"];
+
+console.log(name, courses);
+
+let n = courses.length;
+if (n % 2 == 0) {
+    for (let i = 0; i <= 200; i++) {
+        if (i % 2 == 0) {
+            console.log(i)
+        }
+    }
+};
