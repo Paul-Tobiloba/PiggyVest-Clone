d53fc16b83a91a579a6c2b16337fba8fbd6b1b4e
TobyCodes <twiseas9ice@gmail.com>
2020-04-10 15:42:33 +0100

    Node JS  class 1

--- paulTobiloba/app.js
+++ paulTobiloba/app.js
@@ -1,9 +1,18 @@
-var http = require("http"); http.createServer(function (req, res) {
+var http = require("http");
+http
 
-    res.writeHead(200, { "Content-Type": "text/plain" });
+    .createServer(function (req, res) {
 
-    res.end("Hello World!");
+        console.log(req, res);
 
-})
+        res.writeHead(200, {
+            "Content-Type": "text/plain"
+        });
+
+        res.write("Hello World!");
+
+        res.end();
+
+    })
 
     .listen(8080);
